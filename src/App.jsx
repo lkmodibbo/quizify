2c9e3c345beda4dd1491718e58411bfbd861a146
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-10-04 12:23:09 +0100

    set-up made usig vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <h1>Welcome to Quizify 🎯</h1>
+      <p>Your interactive quiz app is coming soon!</p>
+    </div>
+  )
+}
+
+export default App
