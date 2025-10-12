f5eb5ae430bfb165ad7a2832312a2a835fe55ab5
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-10-12 21:52:39 +0100

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
-import QuizHome from './components/QuizHome'
+import './App.css';
+import QuizHome from './components/QuizHome';
+// 👇 FIX: Import the necessary components from react-router-dom
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 
 function App() {
-  
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<QuizHome /> />
-      <QuizHome />
+        <Route path="/" element={<QuizHome />} /> 
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
